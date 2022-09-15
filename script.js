cd7b29142c3709e9017c43e7619a624045d85329
Oluwatimilehin Esan <107879861+therealtmmy@users.noreply.github.com>
2022-09-15 14:41:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let content = document.getElementById
+("error")
+let message = "Please provide a valid email"
+
+function errorMessage() {
+    content.innerText = message
+}
+
+// Welcome to my Front-end Mentor Challenge
